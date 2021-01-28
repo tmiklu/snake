3e71430118ca4f4507eab50ec74ada29cd3688c0
tmiklu <t__m@azet.sk>
2021-01-28 16:01:22 +0100

    Add x, y array for game plan

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ screen = pygame.display.set_mode((640, 640))
 snake_img = pygame.image.load('snake/square.png')
 snake_x = 0
 snake_y = 0
-snake_move = 0
+snake_move_x = [0, 64, 128, 192, 256, 320, 384, 448, 512, 576]
+snake_move_y = [0, 64, 128, 192, 256, 320, 384, 448, 512, 576]
 
 def snake(x, y):
     screen.blit(snake_img, (x, y))
@@ -22,6 +23,8 @@ GREEN = (0, 255, 0)
 
 
 running = True
+index_x = 0
+index_y = 0
 
 while running:
 
@@ -34,14 +37,24 @@ while running:
         
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                snake_move += 1
+                snake_move_x[index_x]
+                index_x += 1
             if event.key == pygame.K_LEFT:
-                snake_move += -1
+                index_x += -1
+                snake_move_x[index_x]
+            if event.key == pygame.K_DOWN:
+                index_y += 1
+                snake_move_y[index_y]
+            if event.key == pygame.K_UP:
+                index_y += -1
+                snake_move_y[index_y]
         if event.type == pygame.KEYUP:
            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                snake_move = 0
-    
-    snake_x += snake_move
+                #snake_move = 0
+                pass
+
+    snake_x = snake_move_x[index_x]
+    snake_y = snake_move_y[index_y]
 
     snake(snake_x, snake_y)
     pygame.display.update()
